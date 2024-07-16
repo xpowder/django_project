6510b52df1bb19db15abb6e04f641eee48245b2f
xpowder <kadmarmouad8@gmail.com>
2024-07-16 12:50:04 +0100

    hey

---
+++ Mangers/Influ/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+
+
+urlpatterns = [
+    #path('singup/', views.Singup_view, name='singup'),
+    path('home/', views.home),
+    
+    
+    
+    
+    # Includes registration/account management
+
+]
+
---
+++ Mangers/Influ/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+
+
+
+def profile(requset):
+    return render(requset, 'profile.html', {})
+def home(requset):
+    return render(requset, 'home.html', {})
+
+
